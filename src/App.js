2762927707e52b157da1649046537f3690ae43e6
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-30 14:18:00 +0530

    Added: code to display logo using property binding technique

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import logo from './assets/BL_logo.jpg'
 
 class App extends React.Component{ 
   constructor(){ 
@@ -12,6 +13,8 @@ class App extends React.Component{
     return( 
       <div> 
         <h1>{this.state.title}</h1> 
+        <img src={logo} 
+              alt="The BridgeLabz Logo: a Bridge to Employeement through lab works" />
       </div> 
       );
     }
