410cf49ffea32ab7e348edaf04c85c78b429704e
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-30 14:26:47 +0530

    Added: code to launch BL site in new tab on clicking BL logo

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import React from 'react';
 import logo from './assets/BL_logo.jpg'
 
 class App extends React.Component{ 
+  url = 'https://www.bridgelabz.com/'
   constructor(){ 
     super() 
     this.state={ 
       title:'Hello From BridgeLabz' 
     } 
   } 
+
+  //onClick function
+  onClick = ($event) =>{
+    console.log("save button is clicked",$event)
+    window.open(this.url, "_blank")
+  }
+
   render(){
     return( 
       <div> 
         <h1>{this.state.title}</h1> 
-        <img src={logo} 
+        <img src={logo} onClick={this.onClick}
               alt="The BridgeLabz Logo: a Bridge to Employeement through lab works" />
       </div> 
       );
