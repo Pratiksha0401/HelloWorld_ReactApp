e3ed92bcca9a4c6574f073f42c691dc9f35c1a27
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-30 09:10:46 +0530

    Creating Hello world React App to display 'Hello From BridgeLabz'

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component{
+render() {
+  return (
+    <div>
+      <h1>Hello From BridgeLabz</h1>
+    </div>
+    );
+  }
+}
+
+
+export default App;
