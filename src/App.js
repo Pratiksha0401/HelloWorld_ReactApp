e102f640f626a95eb2faea7857c4b5a0f2444137
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-30 09:26:27 +0530

    Added: code to display message using one way data bindings

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import React from 'react';
 
-class App extends React.Component{
-render() {
-  return (
-    <div>
-      <h1>Hello From BridgeLabz</h1>
-    </div>
-    );
-  }
+class App extends React.Component{ 
+  constructor(){ 
+    super() 
+    this.state={ 
+      title:'Hello From BridgeLabz' 
+    } 
+  } 
+  render(){
+    return( 
+      <div> 
+        <h1>{this.state.title}</h1> 
+      </div> 
+      );
+    }
 }
-
-
 export default App;
