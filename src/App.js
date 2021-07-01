8b3aa0528e3140fd2f4e0c9d136a9c3cd43fa418
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-07-01 11:45:55 +0530

    Added: code for define route module path to home component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component{
       <div className="app-main">
         <Router>
           <Switch>
-            <Route path="" >
+            <Route path="/home" >
               <Home />
             </Route>
           </Switch>
